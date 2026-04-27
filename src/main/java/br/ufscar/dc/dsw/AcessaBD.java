e70b561a67ebac3a0a72248f33f519be1d83a98d
Gustavo <gustavobragaia12@gmail.com>
2026-04-27 08:00:22 -0300

    fix folder

---
+++ src/main/java/br/ufscar/dc/dsw/AcessaBD.java
@@ -0,0 +1,63 @@
+package br.ufscar.dc.dsw;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class AcessaBD {
+
+	public static void main(String[] args) {
+		try {
+
+			/* Setup para uso do banco de dados MySQL */
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url = "jdbc:mysql://localhost:3306/Livraria";
+			Connection con = (Connection) DriverManager.getConnection(url, "root", "root");
+
+			/* Setup para uso do banco de dados PostgreSQL */
+
+			/*
+			 * Class.forName("org.postgresql.Driver");
+			 * String url = "jdbc:postgresql://localhost:5432/Livraria";
+			 * Connection con = (Connection) DriverManager.getConnection(url, "root", "root");
+			 */
+			
+			/* Setup para uso do banco de dados Derby */
+
+			/*
+			 * Class.forName("org.apache.derby.jdbc.ClientDriver");
+			 * String url = "jdbc:derby://localhost:1527/Livraria";
+			 * Connection con = (Connection) DriverManager.getConnection(url, "root", "root");
+			 */
+
+			/* Setup para uso do banco de dados SQLite */
+			
+			/*
+			 * Class.forName("org.sqlite.JDBC");
+			 * String url = "jdbc:sqlite:./db/SQLite/Livraria.db";
+			 * Connection con = (Connection) DriverManager.getConnection(url);
+			 */
+
+			Statement stmt = con.createStatement();
+			String query = "SELECT l.titulo, l.autor, l.ano, l.preco, e.nome FROM Livro l, Editora e WHERE l.editora_id = e.id;";
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				System.out.print(rs.getString("titulo"));
+				System.out.print(", " + rs.getString("autor"));
+				System.out.print(", " + rs.getInt("ano"));
+				System.out.print(", " + rs.getString("nome"));
+				System.out.println(" (R$ " + rs.getFloat("preco") + ")");
+			}
+			stmt.close();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			System.out.println("A classe do driver de conexão não foi encontrada!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("O comando SQL não pode ser executado!");
+		}
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page isELIgnored="false"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Livraria Virtual</title>
+    </head>
+    <body>
+        <a href="livros">CRUD Livros</a>
+        <br/>
+        <a href="editoras">CRUD Editoras</a>
+    </body>
+</html>
